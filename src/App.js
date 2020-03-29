1ce63d7375782d757de393ad84fdf8712f649539
hmahida <hitendrasinh.mahida@gmail.com>
2020-03-28 21:34:54 -0400

    Updated app and style using scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {Database} from 'react-feather';
+
+import './App.scss';
+import Home from './components/home';
+import Navbar from './components/navbar';
+import Links from './components/links';
+import Summary from './components/summary';
+import FAQ from './components/faq';
+import Banner from './components/banner';
+
+const history = require('history').createBrowserHistory;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router history={history}>
+        <Route render={({location}) => (
+          <div className="Almighty-Router">
+            <Navbar />
+            <Banner />
+            <Route exact path="/" render={() => <Redirect to="/" />} />
+            <Switch location={location}>
+              <Route exact path="/" render={(props) => <Home {...props}/>} />
+              <Route exact path="/links" render={(props) => <Links {...props}/>} />
+              <Route exact path="/summary" render={(props) => <Summary {...props}/>} />
+              <Route exact path="/faqs" render={(props) => <FAQ {...props}/>} />
+            </Switch>
+          </div>
+        )}
+        />
+      </Router>
+      <footer className="fadeInUp" style={{animationDelay: '2s'}}>
+        <img src="/icon.png" alt="logo"/>
+        <h5>We stand with everyone fighting on the frontlines</h5>
+        <div className="link">
+          <a href="https://github.com/">canadastats</a>
+
+        </div>
+        <div id='footerButtons'>
+          <a className="button" href="https://bit.ly/patientdb" target="_noblank">
+            <Database /><span>Crowd sourced Patient Database</span>
+          </a>
+        </div>
+      </footer>
+
     </div>
   );
 }
